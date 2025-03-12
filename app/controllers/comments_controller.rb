f51804416268823aa4c708b8af9936af6b6d167b
jaythree <giljr.2009@gmail.com>
2025-03-12 17:55:32 -0400

    📡 Three Powerful Methods to Broadcast - Epi 3

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
   
       if @comment.save
         respond_to do |format|
-          format.turbo_stream
+          format.turbo_stream # { turbo_stream.append "comments", partial: "comments/comment", locals: { comment: @comment } }
           format.html { redirect_to @post, notice: "Comment created successfully!" }
         end
       else
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,19 @@ class Comment < ApplicationRecord
 
   validates :content, presence: true
   validates :user, presence: true
-  
-  broadcasts_to :post
+
+  # broadcasts_to :post
+
+  after_create_commit :broadcast_post_comment_create
+
+  private
+
+  def broadcast_post_comment_create
+    broadcast_append_to(
+      post, # The stream target (the post this comment belongs to)
+      partial: 'comments/comment',
+      locals: { comment: self },
+      target: 'comments' # The DOM element ID where the comment will be appended
+    )
+  end
 end
